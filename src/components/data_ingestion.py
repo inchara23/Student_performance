1353cd69d8f52feba0224e7f6cf55021df069892
Inchara <incharaghegde23@gmail.com>
2025-07-07 19:46:38 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-@dataclass                  ## by using this directly we can define without using __init__ method
-class DataIngestionConfig:
+@dataclass                  ## by using this directly we can define without using __init__ method, and we use this
+class DataIngestionConfig:   ## only when we have class variables, if we have any functions, we use mamul init fun
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
@@ -20,7 +20,7 @@ class DataIngestionConfig:
 
 
 class DataIngestion:
-    def __init__(self):
+    def __init__(self):                                  ## this init is nothing but constructor
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
@@ -35,7 +35,7 @@ class DataIngestion:
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            logging.info("Ingestion of the completed")
+            logging.info("Ingestion of the data completed")
 
             return(
                 self.ingestion_config.train_data_path,
